2f6dc63486f7ac413d60f8d848b6fa58e9044424
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-05-27 17:35:44 +0500

    finish

--- js/index.js
+++ js/index.js
@@ -12,19 +12,108 @@
 //   console.log(i);
 // }
 
+// 3 misol
 
-// 3 mislo
+// let rest = 0;
 
-// alert("1 dan n gacha sonlar chufti bilan keyin yegindisi");
-
-// let d = 0;
-// let n = + prompt('son krting');
-// for(let i = 5; i <= d; ){
-//     d = d +i;
-//  console.log(d);
+// alert('1 dan n gacha sonlar yegindisi ')
+// let n = + prompt('son krting')
+//  for( let i = 1; i <= n; i++ ){
+//  rest = rest + i;
 // }
+// console.log(rest)
 
 // 4 misol
+// let rest = 0
+// alert('son krting')
+// let n = + prompt('son krting');
+// for( let i = 0; i<=n; i= i +2){
+// rest = rest + i;
+// }
+// console.log(rest);
 
-//  
+// 5 misol
 
+// let rest = 0;
+// let n = +prompt('son krting');
+// for(i = 1; i<=n ; i = i+ 2){
+// rest = rest + 2;
+// }
+// console.log(rest);
+
+// 6 misol
+
+
+// let n = +prompt('son krting')
+// for(i = 1; i<=n; i++){
+//  if(n % i==0){
+
+//   console.log(i);
+// }
+// }
+
+// 7 misol
+
+// let n = + prompt('son krting');
+// let yegindi = 0;
+// let son = 0;
+
+// for( i = 1; i<=n; i++){
+// if(n%i == 0){
+//  yegindi =yegindi+i;
+//  son =son +1;
+//  console.log(i);
+// }
+// }
+// console.log(son,yegindi);
+
+// let q = +prompt("son kriting");
+// for (let i = 1; i <= q; i++) {
+//   for (let m = 1; m <= i; i++) {
+//     if (m % i != 0|| m/1==m ||m/m==1) {
+//       console.log("tubson");
+//     } else {
+//       console.log("tubson emas");
+//     }
+//   }
+// }
+
+// 9 misol
+
+
+// let n = 10;
+// let k = 5;
+// for( let i = 1; i<=n; i++){
+// console.log(k);
+// }
+
+// 10 misol
+
+// let a = 5;
+// let b =10;
+// let son = 0;
+// for(let i = a; i<=b; i ++){
+//  son = son +1;
+//  console.log(i);
+// }
+// console.log(son);
+
+// 11 mispl
+
+// let a = 5;
+// let b =10;
+// let son = 0
+
+// b=b-1
+// for(let i = b; i > a; i--){
+//     son = son +1;
+//     console.log(i);
+// }
+// console.log(son);
+
+// 12 misol
+     let kg = 10;
+
+     for( i = 1; i <=n; i++){
+     
+    }
