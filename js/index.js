f4a923efb37a67c548d129a12fcd8dc684a96ecb
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-05-27 09:42:26 +0500

    for

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// 1 misol
+
+// alert("1 dan n gacha son lar chiqarilsni");
+// for(let i = 1; i<=10; i++) {
+//  console.log(i);
+// }
+
+// 2 misol
+
+// alert("n dan 1 gacha sonlar ciqarilsin");
+// for(let i = 10; i >= 1; i--){
+//   console.log(i);
+// }
+
+
+// 3 mislo
+
+// alert("1 dan n gacha sonlar chufti bilan keyin yegindisi");
+
+// let d = 0;
+// let n = + prompt('son krting');
+// for(let i = 5; i <= d; ){
+//     d = d +i;
+//  console.log(d);
+// }
+
+// 4 misol
+
+//  
+
